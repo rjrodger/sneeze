0480f4eff7d806ea7ca09a5ff6a5b4b4835511e7
rjrodger <richard@ricebridge.com>
2016-07-12 12:20:02 -0400

    base host

--- test/base.js
+++ test/base.js
@@ -2,16 +2,18 @@
 // $ node base.js
 
 // multi-base network:
-// $ node base.js b0 39000 127.0.0.1:39000,127.0.0.1:39001
-// $ node base.js b1 39001 127.0.0.1:39000,127.0.0.1:39001
+// $ node base.js b0 127.0.0.1 39000 127.0.0.1:39000,127.0.0.1:39001
+// $ node base.js b1 127.0.0.1 39001 127.0.0.1:39000,127.0.0.1:39001
 
 var NAME = process.env.NAME || process.argv[2] || 'b0'
-var PORT = process.env.PORT || process.argv[3] || 39000
-var BASES = (process.env.BASES || process.argv[4] || '127.0.0.1:39000').split(',')
+var HOST = process.env.HOST || process.argv[3] || '127.0.0.1'
+var PORT = process.env.PORT || process.argv[4] || 39000
+var BASES = (process.env.BASES || process.argv[5] || '127.0.0.1:39000').split(',')
 
 require('..')({
   isbase: true, 
   silent: false, 
+  host: HOST,
   port: PORT, 
   bases: BASES
 }).join({name: NAME})
